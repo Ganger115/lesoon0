da00e0a04287d4df105505d5626b3a7d2081f236
Алексей <108700816+Ganger115@users.noreply.github.com>
2024-11-01 01:29:54 +0300

    Динамическая типизация
    Написать программу на языке Python, используя Pycharm, для демонстрации динамической типизации.

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+Name = "Alexseu"
+print(Name , type(Name))
+Age = 25
+print(Age , type(Age))
+New_Age = Age + 1
+print(New_Age , type(New_Age))
+is_student = 2 > 1
+print(is_student , type(is_student))
