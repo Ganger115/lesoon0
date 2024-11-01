361a0e951c99a827cf4a232a59fc566110cafe26
Алексей <108700816+Ganger115@users.noreply.github.com>
2024-11-02 01:17:58 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+number_of_completed = 3
+number_of_hours = 1.5
+course_name = "Module 1"
+time_per_task = number_of_hours / number_of_completed
+print("Курс:" , course_name , "всего задач:" ,  number_of_completed , "затрачено часов:" , number_of_hours , "Среднее время выполнения" , time_per_task , "часа.")
